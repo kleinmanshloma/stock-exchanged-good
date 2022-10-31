b7c60094e7cdc9ccda1ceb71e04beb0daea6f8bb
kleinmanshloma <skwebdeveloper.net@gmail.com>
2022-11-01 00:10:01 +0100

    started to work on buy and sell

---
+++ script-object.js
@@ -0,0 +1,32 @@
+const COMPANIES = [
+  {
+    name: "bp",
+    ticker: "BP",
+    price: 31.36,
+    isUsCompany: true,
+  },
+  {
+    name: "eni",
+    ticker: "E",
+    price: 23.85,
+    isUsCompany: false,
+  },
+  {
+    name: "Royal Dutch Shell",
+    ticker: "RDS.A",
+    price: 51.04,
+    isUsCompany: true,
+  },
+  {
+    name: "TotalEnergies",
+    ticker: "TTE",
+    price: 52.18,
+    isUsCompany: false,
+  },
+  {
+    name: "Linde",
+    ticker: "LIN",
+    price: 288.32,
+    isUsCompany: true,
+  },
+];
--- script.js
+++ script.js
@@ -1,28 +1,78 @@
-// declare and init var
-let firstCompanyName = "BP";
-let firstCompanyPrice = 31.36;
+const WE_CURRENTLY_SUPPORT = document.querySelector(".we-support");
+const COMPANY_NAME = document.querySelector(".company-name");
+
+// THIS 2 IS NOT WORKING IN THE FUNCTION BELOW!!!!!!
+const SHARE_NAME_INSERT = document.querySelector(".share-name").value;
+const SHARES_AMOUNT = document.querySelector(".Purchase-amount").value;
+/* const DEPOSIT_AMOUNT_INSERT = document.querySelector(".deposit-amount").value; */
+
 let numbersOfSharesToBuy;
 let totalPrice;
 let dollarAmountToDeposit;
 let hasEnoughMoney;
+let isInValidInput;
 
-const WELCOME_MESSAGE = `Welcome to our Stock Exchange Program!`;
-const FIRST_QUESTION = `How many shares of ${firstCompanyName}, would you like to purchase?`;
+let displayCompanies = [];
 
-// ask user how many stocks do you like to purchase
-numbersOfSharesToBuy = prompt(WELCOME_MESSAGE + "\n" + FIRST_QUESTION);
-console.log(`${typeof +firstCompanyName}`);
-totalPrice = +numbersOfSharesToBuy * firstCompanyPrice;
+for (let company of COMPANIES) {
+  displayCompanies.push(company.name);
+  console.log(...displayCompanies);
+}
+/* console.log(displayCompanies.join(", ")); */
+const COMPANIES_NAMES = ` ${displayCompanies.join("<p class=mb-1>")}`;
+
+const WE_SUPPORT_MESSAGE = `We currently soppurt ${displayCompanies.length} companies`;
+
+WE_CURRENTLY_SUPPORT.innerHTML = `${WE_SUPPORT_MESSAGE} `;
+
+COMPANY_NAME.innerHTML = `<p>${COMPANIES_NAMES} `;
+
+document.getElementById("buy/sell").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let result = COMPANIES.find(
+    (o) => o.name === document.querySelector(".share-name").value
+  );
+  let sharePrice = result.price;
+
+  let totalPrice =
+    sharePrice * document.querySelector(".Purchase-amount").value;
+  alert(`Amount needed for this transaction is $${totalPrice}`);
+});
+
+// ${} <p>
+/* companyIndex = prompt(
+  `${WELCOME_MESSAGE} Which company would you like to purchase `
+);
+
+companyIndex--;
+
+const HOW_MANY_SHARES = `How many shares of ${companyIndex}, would you like to purchase?`;
+
+numbersOfSharesToBuy = prompt(HOW_MANY_SHARES); */
+
+totalPrice = +numbersOfSharesToBuy * COMPANIES[0].price;
+
+isInValidInput = isNaN(numbersOfSharesToBuy);
+
+/* 
+if (isInValidInput) {
+  alert(`Please enter a valid number!`);
+  throw "in valid input";
+}
 
 dollarAmountToDeposit = prompt(
   `Total price is: $${totalPrice}. How mutch do you want to deposit?`
-);
-dollarAmountToDeposit = +dollarAmountToDeposit;
-console.log(`${typeof +dollarAmountToDeposit}`);
+  );
+  
+  dollarAmountToDeposit = +dollarAmountToDeposit;
+  console.log(`${typeof +dollarAmountToDeposit}`);
 
-hasEnoughMoney = totalPrice > dollarAmountToDeposit;
-
-if (!hasEnoughMoney) {
-  let notgo = "njkl";
-  console.log(`true`);
-}
+  hasEnoughMoney = totalPrice < dollarAmountToDeposit;
+  
+  if (hasEnoughMoney) {
+    alert(`purchased!`);
+  } else {
+    alert(`Not enough funds to make the transaction!`);
+  }
+  */
