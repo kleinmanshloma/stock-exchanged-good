123fa43734b21bf08aaaec60b7ec391de748d972
JOELjacob2523 <Shlomejacob@gmail.com>
2022-11-08 12:43:25 -0500

    joel

--- script-object.js
+++ script-object.js
@@ -10,25 +10,37 @@ const COMPANIES = [
     isUsCompany: true,
   },
   {
-    name: "eni",
+    name: `<a
+    href="https://www.marketbeat.com/stocks/NYSE/E/"
+    target="_blank"
+    >eni</a>`,
     ticker: "E",
     price: 23.85,
     isUsCompany: false,
   },
   {
-    name: "Royal Dutch Shell",
+    name: `<a
+    href="https://www.marketbeat.com/stocks/NYSE/RDS-A/"
+    target="_blank"
+    >Royal Dutch Shell</a>`,
     ticker: "RDS.A",
     price: 51.04,
     isUsCompany: true,
   },
   {
-    name: "TotalEnergies",
+    name: `<a
+    href="https://www.marketbeat.com/stocks/NYSE/TTE/"
+    target="_blank"
+    >TotalEnergies</a>`,
     ticker: "TTE",
     price: 52.18,
     isUsCompany: false,
   },
   {
-    name: "Linde",
+    name: `<a
+    href="https://www.marketbeat.com/stocks/NYSE/LIN/"
+    target="_blank"
+    >Linde</a>`,
     ticker: "LIN",
     price: 288.32,
     isUsCompany: true,
